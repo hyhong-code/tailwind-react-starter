2dceebab740661ca22385e4051326788ad64d01c
Haiyang Hong <hong961127@gmail.com>
2020-11-04 13:19:42 -0800

    Add headless ui as dependency

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// Extending defualt config
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff6363",
+        secondary: {
+          100: "#e2e2d5",
+          200: "#888883",
+        },
+      },
+      fontFamily: {
+        body: ["Nunito"], // will generate class name: font-body
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
